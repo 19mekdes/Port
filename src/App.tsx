6325034816d078b14becbf3ccae24b1b8615061d
mekdes wale <mekdesw60@gmail.com>
2026-03-05 11:26:11 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl  bg-color ring-emerald-400 font-bold">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
