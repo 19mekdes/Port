931fefe3372faea1815ca3a49c7bcd029e597818
mekdes wale <mekdesw60@gmail.com>
2026-03-14 16:59:50 +0300

    rename project

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { useEffect, useRef, useState } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+import Projects from './components/project';
+import Skills from './components/Skill';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
